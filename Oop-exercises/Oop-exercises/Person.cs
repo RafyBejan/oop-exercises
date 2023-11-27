c03f25d0f30f134b8cf63eff26c6d88c8456fe38
bejan <bejan@RAFAEL12>
2023-11-27 09:50:38 +0200

    initial setup exercises

---
+++ Oop-exercises/Oop-exercises/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop_exercises
+{
+    internal class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public DateTime DateofBirth { get; set; }
+        public int Age
+        { get
+            {
+                return DateTime.Now.Year - DataOfBirth.Year;
+
+            }
+        }
+
+            
+            
+
+    
+}
---
+++ Oop-exercises/Oop-exercises/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Oop_exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
